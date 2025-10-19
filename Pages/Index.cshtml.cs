7889a0ab54f09ef4f176d99c59e1e7a8f8baea9f
Ian Medina <ianchuloooo@gmail.com>
2025-10-19 13:22:31 -0300

    Initial

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace HBoostUltra.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Aquí puedes inicializar datos si quieres pasar algo al HTML.
+            // Por ejemplo, podrías cargar configuraciones desde la base de datos.
+        }
+    }
+}
